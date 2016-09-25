8ce35e3573f5c95954b22f9a49230efb1831328b
Ashris Choudhury <ashris.me@gmail.com>
2016-09-25 12:48:31 +0530

    remove three

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ $(".introscroll").click(function() {
         scrollTop: $("#mbody").offset().top
     }, 1200);
 });
+
+
+
+
+$('.xelec').click(function(){
+$('.pitem').addClass("hidden");
+$('.elec').removeClass("hidden");
+});
+$('.xarch').click(function(){
+$('.pitem').addClass("hidden");
+$('.vizu').removeClass("hidden");
+});
+$('.xrese').click(function(){
+$('.pitem').addClass("hidden");
+$('.rese').removeClass("hidden");
+});
+$('.xprog').click(function(){
+$('.pitem').addClass("hidden");
+$('.soft').removeClass("hidden");
+});
+$('.xall').click(function(){
+$('.pitem').removeClass("hidden");
+});
